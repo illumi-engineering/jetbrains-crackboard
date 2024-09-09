e9a37867eeaf1d6e1d080aa9c8d7631d3f914928
Liz Ainslie <lizainslie16@gmail.com>
2024-09-09 02:31:49 -0700

    fix: statusbar data fix
    was gonna enable it but need a public facing api for how much time has been spent total

--- src/main/kotlin/sh/illumi/labs/jetbrains_crackboard/CrackBoard.kt
+++ src/main/kotlin/sh/illumi/labs/jetbrains_crackboard/CrackBoard.kt
@@ -1,11 +1,11 @@
 package sh.illumi.labs.jetbrains_crackboard
 
 import com.intellij.AppTopics
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.diagnostic.runAndLogException
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.fileEditor.FileDocumentManager
@@ -14,14 +14,6 @@ import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.WindowManager
-import com.intellij.util.progress.sleepCancellable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
 import kotlinx.datetime.toKotlinInstant
 import sh.illumi.labs.jetbrains_crackboard.listeners.*
 import sh.illumi.labs.jetbrains_crackboard.ui.SessionKeyDialog
@@ -34,15 +26,17 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toKotlinDuration
 
 @Service
-class CrackBoard(
-    private val cs: CoroutineScope
-) {
+class CrackBoard : Disposable {
     private val log = logger<CrackBoard>()
     private var lastHeartbeatTime: LocalDateTime? = null
-    private lateinit var currentLanguage: String
+    private var currentLanguage: String? = null
+    private var statusBarHeartbeat: LocalDateTime? = null
+    private var lastStatusBarUpdate: LocalDateTime? = null
 
     init {
         setupStatusBar()
@@ -52,7 +46,14 @@ class CrackBoard(
     fun sendHeartbeat(file: VirtualFile) {
         val now = LocalDateTime.now()
 
-        // ensure only send a heartbeat once per interval
+        // only update statusbar every 15 seconds
+        if (lastStatusBarUpdate == null || (now.toInstant(ZoneOffset.UTC).toKotlinInstant() - lastStatusBarUpdate!!.toInstant(ZoneOffset.UTC).toKotlinInstant()) >= STATUSBAR_UPDATE_INTERVAL) {
+            statusBarHeartbeat = now
+            lastStatusBarUpdate = now
+            currentLanguage = file.fileType.name
+        }
+
+        // ensure to only send a heartbeat once per interval
         if (lastHeartbeatTime == null || (now.toInstant(ZoneOffset.UTC).toKotlinInstant() - lastHeartbeatTime!!.toInstant(ZoneOffset.UTC).toKotlinInstant()) >= HEARTBEAT_INTERVAL) {
             val jsonData = """
                 {
@@ -62,42 +63,45 @@ class CrackBoard(
                 }
             """.trimIndent()
 
-            // URL of the API endpoint
-            val url = URI("${service<CrackBoardSettings>().state.baseUrl}/heartbeat").toURL()
-            // Open connection
-            val conn = url.openConnection() as HttpURLConnection
-            conn.requestMethod = "POST"
-            conn.doOutput = true
-            conn.setRequestProperty("Content-Type", "application/json")
+            try {
+                // URL of the API endpoint
+                val url = URI("${service<CrackBoardSettings>().state.baseUrl}/heartbeat").toURL()
+                // Open connection
+                val conn = url.openConnection() as HttpURLConnection
+                conn.requestMethod = "POST"
+                conn.doOutput = true
+                conn.setRequestProperty("Content-Type", "application/json")
 
-            // Write JSON data to the request body
-            conn.outputStream.use { os ->
-                val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
-                writer.write(jsonData)
-                writer.flush()
+                // Write JSON data to the request body
+                conn.outputStream.use { os ->
+                    val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
+                    writer.write(jsonData)
+                    writer.flush()
+                }
+
+                // Get the response code
+                val responseCode = conn.responseCode
+
+                if (responseCode == 429) log.warn("Rate-limited")
+
+                // Read the response
+                val response = conn.inputStream.bufferedReader().use { it.readText() }
+
+                log.debug("Response Code: $responseCode")
+                log.debug("Response: $response")
+
+                log.info("Heartbeat sent successfully.")
+
+                lastHeartbeatTime = now
+            } catch (e: Exception) {
+                log.warn("Failed to send heartbeat", e)
             }
-
-            // Get the response code
-            val responseCode = conn.responseCode
-
-            if (responseCode == 429) log.warn("Rate-limited")
-
-            // Read the response
-            val response = conn.inputStream.bufferedReader().use { it.readText() }
-
-            log.debug("Response Code: $responseCode")
-            log.debug("Response: $response")
-
-            log.info("Heartbeat sent successfully.")
-
-            lastHeartbeatTime = now
-            currentLanguage = file.fileType.name
         }
     }
 
     fun getStatusBarText(): String {
         val duration = getDurationFromLastHeartbeat()
-        return "$currentLanguage: $duration"
+        return "${currentLanguage ?: "none"}: $duration"
     }
 
     val isAppActive get() = KeyboardFocusManager.getCurrentKeyboardFocusManager().activeWindow != null
@@ -117,6 +121,8 @@ class CrackBoard(
             EditorFactory.getInstance().eventMulticaster.addCaretListener(CaretChangeListener(), disposable)
             EditorFactory.getInstance().eventMulticaster.addEditorMouseListener(MouseEventListener(), disposable)
             EditorFactory.getInstance().eventMulticaster.addVisibleAreaListener(VisibilityListener(), disposable)
+
+            Disposer.register(this, disposable)
         }
     }
 
@@ -128,7 +134,7 @@ class CrackBoard(
 
     private val currentProject get() = ProjectManager.getInstance().defaultProject
 
-    private fun getDurationFromLastHeartbeat(time: LocalDateTime = LocalDateTime.now()) = java.time.Duration.between(lastHeartbeatTime, time).toKotlinDuration()
+    private fun getDurationFromLastHeartbeat(time: LocalDateTime = LocalDateTime.now()) = java.time.Duration.between(statusBarHeartbeat ?: time, time).toKotlinDuration()
 
     private val sessionKey get() =
         service<CrackBoardSettings>().state.sessionKey ?: run {
@@ -137,6 +143,11 @@ class CrackBoard(
         }
 
     companion object {
-        val HEARTBEAT_INTERVAL = (2 * 60 * 1000).milliseconds // 2 minutes
+        val HEARTBEAT_INTERVAL = 2.minutes
+        val STATUSBAR_UPDATE_INTERVAL = 15.seconds
+    }
+
+    override fun dispose() {
+        log.info("Disposing CrackBoard")
     }
 }
